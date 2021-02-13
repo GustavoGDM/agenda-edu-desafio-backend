08f2921cee1759517f653a6eefe03c79f1a50d10
Gustavo Duarte <gustavo.duarte@rupee.com.br>
2021-02-13 02:28:11 -0300

    [AG-EDU-4] Refine upload file and add ordering Parliamentarians

--- app/controllers/upload_files_controller.rb
+++ app/controllers/upload_files_controller.rb
@@ -4,14 +4,12 @@ class UploadFilesController < ApplicationController
   before_action :set_upload_file, only: [:show]
   
   def create
-    redirect_to root_path unless valid_file?
-    file = UploadFile.new(name: file_params[:file].original_filename, path: file_params[:file].path)
-    save_upload_file(file) if file.read_file
-    if file.new_record?
-      redirect_to root_path, flash: { error: 'Não foi possivel carregar o arquivo.' }
-    else
-      redirect_to upload_file_path(file)
+    if valid_file?
+      file = UploadFile.new(name: file_params[:file].original_filename, path: file_params[:file].path)
+      save_upload_file(file) if file.read_file
+      return redirect_to upload_file_path(file) unless file.new_record?
     end
+    redirect_to root_path, flash: { error: 'Não foi possivel carregar o arquivo.' } 
   end
 
   def show
@@ -30,12 +28,24 @@ class UploadFilesController < ApplicationController
   end
 
   def set_upload_file
-    @upload_file = UploadFile.includes(:parliamentarians).find(params[:id])
+    total_parliamentary_expenditure =
+      ParliamentaryExpenditure.joins(:parliamentarian)
+                              .where(parliamentarians: { upload_file_id: params[:id] })
+                              .group(:parliamentarian_id)
+                              .sum(:vlrLiquido)
+                              .to_h
+
+    @parliamentarians = Parliamentarian.where(upload_file_id: params[:id]).map do |p|
+      [p, total_parliamentary_expenditure[p.id]]
+    end
+
+    @parliamentarians = @parliamentarians.sort_by { |_key, value| -value }
   end
 
   def save_upload_file(file)
     UploadFile.transaction do
-      file.save
+      raise ActiveRecord::Rollback unless file.save
+
       parliamentarian_id_list = file.rows.map { |row| row[:ideCadastro] }.uniq
 
       parliamentarian_id_list.each do |ide_cadastro|
@@ -45,9 +55,10 @@ class UploadFilesController < ApplicationController
         parliamentarian = file.parliamentarians.create(**parliamentarian_attrs)
 
         parliamentary_expenditure_attrs = parliamentarian_rows.map do |row|
-          row.slice(:datEmissao, :txtFornecedor, :vlrLiquido, :ideDocumento, :urlDocumento)
+          hash = row.slice(:datEmissao, :txtFornecedor, :vlrLiquido, :ideDocumento, :urlDocumento)
+          hash.merge!({ parliamentarian_id: parliamentarian.id, created_at: Time.now.utc, updated_at: Time.now.utc})
         end
-        parliamentarian.parliamentary_expenditures.create(parliamentary_expenditure_attrs)
+        parliamentarian.parliamentary_expenditures.insert_all!(parliamentary_expenditure_attrs)
         raise ActiveRecord::Rollback if parliamentarian.parliamentary_expenditures.count != parliamentary_expenditure_attrs.count
       end
       raise ActiveRecord::Rollback if file.parliamentarians.count != parliamentarian_id_list.count
