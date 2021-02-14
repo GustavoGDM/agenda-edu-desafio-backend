a3a0e558efcad350a2242ddd007b2625dae9c873
Gustavo Duarte <gustavo.duarte@rupee.com.br>
2021-02-14 01:02:35 -0300

    [AG-EDU-5] refactoring UploadFile component

--- app/controllers/upload_files_controller.rb
+++ app/controllers/upload_files_controller.rb
@@ -4,10 +4,10 @@ class UploadFilesController < ApplicationController
   before_action :set_upload_file, only: [:show]
   
   def create
-    if valid_file?
-      file = UploadFile.new(name: file_params[:file].original_filename, path: file_params[:file].path)
-      save_upload_file(file) if file.read_file
-      return redirect_to upload_file_path(file) unless file.new_record?
+    @file = UploadFile.new(name: file_params[:file].original_filename, file:file_params[:file].tempfile)
+    if @file.is_valid?
+      save_upload_file if @file.read_file
+      return redirect_to upload_file_path(@file) unless @file.new_record?
     end
     redirect_to root_path, flash: { error: 'Não foi possivel carregar o arquivo.' } 
   end
@@ -19,12 +19,6 @@ class UploadFilesController < ApplicationController
 
   def file_params
     params.permit(:file, :authenticity_token, :commit)
-  end
-
-  def valid_file?
-    file_params[:file].present? &&
-      file_params[:file].tempfile.present? &&
-      file_params[:file].tempfile.path['.csv'].length == 4
   end
 
   def set_upload_file
@@ -42,17 +36,17 @@ class UploadFilesController < ApplicationController
     @parliamentarians = @parliamentarians.sort_by { |_key, value| -value }
   end
 
-  def save_upload_file(file)
+  def save_upload_file 
     UploadFile.transaction do
-      raise ActiveRecord::Rollback unless file.save
+      raise ActiveRecord::Rollback unless @file.save
 
-      parliamentarian_id_list = file.rows.map { |row| row[:ideCadastro] }.uniq
+      parliamentarian_id_list = @file.rows.map { |row| row[:ideCadastro] }.uniq
 
       parliamentarian_id_list.each do |ide_cadastro|
-        parliamentarian_rows = file.rows.select { |row| row[:ideCadastro] == ide_cadastro }
+        parliamentarian_rows = @file.rows.select { |row| row[:ideCadastro] == ide_cadastro }
 
         parliamentarian_attrs = parliamentarian_rows.first.slice(:txNomeParlamentar, :cpf, :ideCadastro, :sgPartido)
-        parliamentarian = file.parliamentarians.create(**parliamentarian_attrs)
+        parliamentarian = @file.parliamentarians.create(**parliamentarian_attrs)
 
         parliamentary_expenditure_attrs = parliamentarian_rows.map do |row|
           hash = row.slice(:datEmissao, :txtFornecedor, :vlrLiquido, :ideDocumento, :urlDocumento)
@@ -61,7 +55,10 @@ class UploadFilesController < ApplicationController
         parliamentarian.parliamentary_expenditures.insert_all!(parliamentary_expenditure_attrs)
         raise ActiveRecord::Rollback if parliamentarian.parliamentary_expenditures.count != parliamentary_expenditure_attrs.count
       end
-      raise ActiveRecord::Rollback if file.parliamentarians.count != parliamentarian_id_list.count
+      raise ActiveRecord::Rollback if @file.parliamentarians.count != parliamentarian_id_list.count
     end
   end
 end
+
+
+
--- app/models/upload_file.rb
+++ app/models/upload_file.rb
@@ -2,27 +2,40 @@
 #
 # id:                   integer
 # name:                 string
+# created_at              datetime
+# updated_at              datetime
 
 require 'csv'
 class UploadFile < ApplicationRecord
 
   has_many :parliamentarians
-  attr_accessor :path
-  attr_reader :rows
+  attr_accessor :file, :rows, :path
 
   validates :name, presence: true
 
+  after_initialize do
+    self.path = file.path if file.present?
+  end
+
   def read_file
-    @rows = []
-    CSV.foreach(@path, liberal_parsing: true, col_sep: ';', headers: true) do |row|
-      if row['sgUF'] == 'RJ'
-        @rows << row.to_h.transform_keys do |key|
-          key = key.gsub(/[^0-9A-Za-z]/, '') if key.include?("\"")
-          key.to_sym
+    self.rows = []
+    begin
+      CSV.foreach(path, liberal_parsing: true, col_sep: ';', headers: true) do |row|
+        if row['sgUF'] == 'RJ'
+          rows << row.to_h.transform_keys do |key|
+            key = key.gsub(/[^0-9A-Za-z]/, '') if key.include?("\"")
+            key.to_sym
+          end
         end
       end
+    rescue e
+      return false
     end
-    @rows.any?
+    rows.any?
+  end
+
+  def is_valid?
+    file.present? && path['.csv'].length == 4
   end
 
 end
